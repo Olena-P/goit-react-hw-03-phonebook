3d9cebc0333789a89a3385c5f6d30166e74ff2f0
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2023-11-12 11:33:25 +0200

    Add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,20 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     const { contacts } = this.state;
 
